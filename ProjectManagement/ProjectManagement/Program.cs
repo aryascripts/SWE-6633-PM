8b6648cf416e7eb1013e4eecdcfcd8d1f6ac154e
Aman Bhimani <amanb014@gmail.com>
2016-11-03 15:18:57 -0400

    Adding LiteDB and also added Startup.cs.

--- ProjectManagement/ProjectManagement/Program.cs
+++ ProjectManagement/ProjectManagement/Program.cs
@@ -26,34 +26,6 @@ namespace ProjectManagement
 			public bool IsActive { get; set; }
 		}
 
-		// Open database (or create if doesn't exist)
-using(var db = new LiteDatabase(@"C:\Temp\MyData.db"))
-{
-    // Get a collection (or create, if doesn't exist)
-    var col = db.GetCollection<Customer>("customers");
-
-	// Create your new customer instance
-	var customer = new Customer {
-		Name = "John Doe",
-		Phones = new string[] { "8000-0000", "9000-0000" },
-		IsActive = true
-	};
-
-	// Insert new customer document (Id will be auto-incremented)
-	col.Insert(customer);
-
-    // Update a document inside a collection
-    customer.Name = "Joana Doe";
-
-    col.Update(customer);
-
-    // Index document using document Name property
-    col.EnsureIndex(x => x.Name);
-
-    // Use LINQ to query documents
-    var results = col.Find(x => x.Name.StartsWith("Jo"));
-}
-
 	}
 
 	
